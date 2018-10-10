be8dba1cffda326e146b7f13711e34480e1a5db2
gfvalenzuela <gustavo.valenzuela@nih.gov>
2018-10-10 17:20:41 -0400

    first commit

---
+++ api/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var recurrenceRouter = require('./routes/recurrence');
+const uuid = require('uuid/v1');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use( (req,res,next) => {
+  req.requestId = uuid();
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/recurrence', recurrenceRouter);
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  console.error(err);
+  res.status(500).send({message: "system error"});
+});
+
+module.exports = app;
---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://analysistools.nci.nih.gov/recurrencerisktool'
+};
