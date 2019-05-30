79c2b48c903eca7bdb11f75b30fbc8314fd4de7d
zaki Abdullah <zabdullah94@gmail.com>
2019-05-30 16:41:23 -0400

    Add folder symlinks

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var helmet = require('helmet');
 var cors = require('cors');
+var mkdirp = require('mkdirp');
+
 
 var logger = require('./utils/loggerUtil').logger;
 var indexRouter = require('./routes/index');
@@ -30,12 +32,26 @@ app.get('/ping', (req, res) => {
   res.send('true');
 });
 
+mkdirp('./logs/', function (err) {
+  if (err) console.error(err)
+  else console.log('log folder created')
+});
+mkdirp('./data/staging/', function (err) {
+  if (err) console.error(err)
+  else console.log('data folder created')
+});
+mkdirp('./public/', function (err) {
+  if (err) console.error(err)
+  else console.log('public folder created')
+});
+
+
 app.use(function (err, req, res, next) {
   logger.log('error',err);
   res.status(500).send({errors: [ {
     msg:'An unexpected error occured. Please ensure the input file(s) is in the correct format and/or correct parameters were chosen.'} ]});
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
 
 module.exports = app;
