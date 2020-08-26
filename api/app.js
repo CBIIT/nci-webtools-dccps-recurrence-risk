79124f59beb57f42628bc373c96f66e5a8d63be7
Brian Park <park-brian@users.noreply.github.com>
2020-08-26 01:16:42 -0400

    update hsts policy

--- api/app.js
+++ api/app.js
@@ -13,7 +13,13 @@ const uuid = require('uuid/v1');
 
 var app = express();
 
-app.use(helmet());
+app.use(helmet({
+  hsts: {
+    maxAge: 31536000,
+    includeSubDomains: true,
+    preload: true,
+  }
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
