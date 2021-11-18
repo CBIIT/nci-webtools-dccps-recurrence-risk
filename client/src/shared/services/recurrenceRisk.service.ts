437ac2dee2755ac8ea7961f8c57c3c4d0e7dca1a
Brian Park <park-brian@users.noreply.github.com>
2021-11-18 13:06:24 -0500

    rename folders

---
+++ client/src/shared/services/recurrenceRisk.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+ providedIn: 'root',
+})
+export class RecurrenceRiskService {
+
+  private _currentState: any = {
+	  group: {
+	    form: {},
+	    metadata: {},
+	    data: [{}, {}, {}]
+	  },
+	  individual: {
+	    form: {},
+	    metadata: {},
+	    data: [{}, {}, {}] }
+  };
+
+  constructor() { /*noop*/}
+
+  setCurrentState(key: string,state: any) {
+    this._currentState[key] = state;
+  }
+
+  getCurrentState(key: string) {
+    return this._currentState[key];
+  }
+}
