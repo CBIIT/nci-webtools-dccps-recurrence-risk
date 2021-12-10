3ed5f3d68307f54118fac18b21f52cfc12e3ddf6
Brian Park <park-brian@users.noreply.github.com>
2021-12-09 23:57:15 -0500

    add default values for individual data

---
+++ client/src/app/pages/individual-data/individual-data.defaults.ts
@@ -0,0 +1,23 @@
+import { IndividualDataParameters, IndividualDataWorkspace } from "./individual-data.types";
+
+export const DEFAULT_INDIVIDUAL_DATA_PARAMETERS: IndividualDataParameters = {
+  individualData: [],
+  individualDataFileName: "",
+  individualDataHeaders: [],
+  strata: [],
+  covariates: [],
+  timeVariable: "",
+  eventVariable: "",
+  distribution: "",
+  stageVariable: "",
+  distantStageValue: 0,
+  adjustmentFactorR: 1,
+  followUpYears: 25,
+  queue: false,
+  email: "",
+};
+
+export const DEFAULT_INDIVIDUAL_DATA_WORKSPACE: IndividualDataWorkspace = {
+  parameters: DEFAULT_INDIVIDUAL_DATA_PARAMETERS,
+  results: [],
+};
