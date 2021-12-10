68e591be7404c5199c2c0596f65adf0cb06d6d1b
Brian Park <park-brian@users.noreply.github.com>
2021-12-09 22:38:54 -0500

    add type information

--- client/src/app/services/recurrence/recurrence.service.ts
+++ client/src/app/services/recurrence/recurrence.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { GroupDataParameters } from "src/app/pages/group-data/group-data-form/group-data-form.component";
+import { Row } from "src/app/components/table/table.component";
 
 @Injectable({
   providedIn: "root",
@@ -9,10 +10,10 @@ export class RecurrenceService {
   constructor(private http: HttpClient) {}
 
   getRiskFromGroupData(parameters: GroupDataParameters) {
-    return this.http.post("api/v2/risk/group-data", parameters);
+    return this.http.post<Row[]>("api/v2/risk/group-data", parameters);
   }
 
   getRiskFromIndividualData(parameters: any) {
-    return this.http.post("api/v2/risk/individual-data", parameters);
+    return this.http.post<Row[]>("api/v2/risk/individual-data", parameters);
   }
 }
