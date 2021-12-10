d54774c2cc0e6f390b6886ed88edc4c6d38a560c
Brian Park <park-brian@users.noreply.github.com>
2021-12-09 22:40:54 -0500

    use consistent method names for validator

--- client/src/app/pages/group-data/group-data-form/validators.ts
+++ client/src/app/pages/group-data/group-data-form/validators.ts
@@ -1,6 +1,6 @@
 import { ValidatorFn, AbstractControl, ValidationErrors } from "@angular/forms";
 
-export const seerStatFilesValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+export const seerStatDataFilesValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const files: File[] = Array.from(control.value || []);
   const isValid =
     files?.find((file: File) => /.dic$/i.test(file.name)) && files?.find((file: File) => /.txt$/i.test(file.name));
