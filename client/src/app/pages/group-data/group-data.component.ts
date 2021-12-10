000119bf80cdcf0576ced11bde8c8519e5cbd2c7
Brian Park <park-brian@users.noreply.github.com>
2021-12-09 22:41:51 -0500

    implement restoring results from workspace

--- client/src/app/pages/group-data/group-data.component.ts
+++ client/src/app/pages/group-data/group-data.component.ts
@@ -1,46 +1,43 @@
 import { Component, OnInit } from "@angular/core";
-import { Row } from "src/app/components/table/table.component";
+import { lastValueFrom, Observable } from "rxjs";
 import { RecurrenceService } from "src/app/services/recurrence/recurrence.service";
+import { GroupDataParameters } from "./group-data-form/group-data-form.component";
+import { GroupDataWorkspace } from "./group-data-workspace";
 
 @Component({
   selector: "app-group-data",
   templateUrl: "./group-data.component.html",
   styleUrls: ["./group-data.component.scss"],
 })
-export class GroupDataComponent implements OnInit {
-  results: Row[] = [];
+export class GroupDataComponent {
   error: any = null;
   loading: boolean = false;
-  parameters: any = null;
+  workspace: GroupDataWorkspace = new GroupDataWorkspace();
 
   constructor(private recurrenceRiskService: RecurrenceService) {}
 
-  ngOnInit(): void {
-    this.handleReset();
-  }
-
   handleReset() {
-    this.results = [];
+    this.workspace = new GroupDataWorkspace();
     this.error = null;
     this.loading = false;
   }
 
-  async handleSubmit(parameters: any) {
+  async handleSubmit(parameters: GroupDataParameters) {
     try {
-      this.error = null;
+      this.handleReset();
       this.loading = true;
-      this.parameters = parameters;
-
-      if (parameters.results) {
-        this.results = parameters.results;
-      } else {
-        this.results = (await this.recurrenceRiskService.getRiskFromGroupData(parameters).toPromise()) as any;
-      }
+      const response$ = this.recurrenceRiskService.getRiskFromGroupData(parameters);
+      const results = await lastValueFrom(response$);
+      this.workspace = new GroupDataWorkspace(parameters, results);
     } catch (e) {
       this.error = e;
-      console.log(e);
+      console.error(e);
     } finally {
       this.loading = false;
     }
+  }
+
+  handleLoadWorkspace(workspace: GroupDataWorkspace) {
+    this.workspace = workspace;
   }
 }
