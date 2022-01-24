6e6469697096952e29126a489f8077d9861d41ce
Brian Park <park-brian@users.noreply.github.com>
2022-01-23 19:32:14 -0500

    use import.meta.url for script-relative paths

--- server/services/recurrence.js
+++ server/services/recurrence.js
@@ -1,16 +1,18 @@
-const path = require("path");
-const r = require("r-wrapper").async;
-const sourcePathV1 = path.resolve(__dirname, "recurrenceV1.R");
-const sourcePathV2 = path.resolve(__dirname, "recurrenceV2.R");
+import { fileURLToPath } from "url";
+import r from "r-wrapper";
 
-module.exports = {
-  ping: () => r(sourcePathV2, "ping"),
+const sourcePathV1 = fileURLToPath(new URL("recurrenceV1.R", import.meta.url));
+const sourcePathV2 = fileURLToPath(new URL("recurrenceV1.R", import.meta.url));
+
+export const recurrence = {
+  ping: () => r.async(sourcePathV2, "ping"),
+  shouldQueue: ({ covariates, strata }) => covariates.length > 0 || strata.length > 2,
   v1: {
-    getRiskFromGroupData: (params) => r(sourcePathV1, "getRiskFromGroupData", params),
-    getRiskFromIndividualData: (params) => r(sourcePathV1, "getRiskFromIndividualData", params),
+    getRiskFromGroupData: (params) => r.async(sourcePathV1, "getRiskFromGroupData", params),
+    getRiskFromIndividualData: (params) => r.async(sourcePathV1, "getRiskFromIndividualData", params),
   },
   v2: {
-    getRiskFromGroupData: (params) => r(sourcePathV2, "getRiskFromGroupData", params),
-    getRiskFromIndividualData: (params) => r(sourcePathV2, "getRiskFromIndividualData", params),
+    getRiskFromGroupData: (params) => r.async(sourcePathV2, "getRiskFromGroupData", params),
+    getRiskFromIndividualData: (params) => r.async(sourcePathV2, "getRiskFromIndividualData", params),
   },
 };
