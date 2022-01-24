f207e4972edc442b9a9b9fdae0b286ffb2288408
Brian Park <park-brian@users.noreply.github.com>
2022-01-24 17:20:25 -0500

    fix typo

--- server/services/email.js
+++ server/services/email.js
@@ -1,16 +1,17 @@
 import { createTransport } from "nodemailer";
 
 export function getSmtpTransport(env = process.env) {
-  const { EMAIL_STMP_HOST, EMAIL_STMP_USER, EMAIL_STMP_PASSWORD } = env;
+  const { EMAIL_SMTP_HOST, EMAIL_SMTP_PORT, EMAIL_SMTP_USER, EMAIL_SMTP_PASSWORD } = env;
 
   let config = {
-    host: EMAIL_STMP_HOST,
+    host: EMAIL_SMTP_HOST,
+    port: EMAIL_SMTP_PORT,
   };
 
-  if (EMAIL_STMP_USER && EMAIL_STMP_PASSWORD) {
+  if (EMAIL_SMTP_USER && EMAIL_SMTP_PASSWORD) {
     config.auth = {
-      user: EMAIL_STMP_USER,
-      pass: EMAIL_STMP_PASSWORD,
+      user: EMAIL_SMTP_USER,
+      pass: EMAIL_SMTP_PASSWORD,
     };
   }
 
