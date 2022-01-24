3b0c5a3713fc8a9235e60bf2763099e7306cf8f3
Brian Park <park-brian@users.noreply.github.com>
2022-01-23 19:33:51 -0500

    support large messages by default

--- server/services/queue.js
+++ server/services/queue.js
@@ -1,49 +1,60 @@
-const crypto = require("crypto");
-const AWS = require("aws-sdk");
+import { randomBytes } from "crypto";
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import {
+  ChangeMessageVisibilityCommand,
+  DeleteMessageCommand,
+  GetQueueUrlCommand,
+  ReceiveMessageCommand,
+  SendMessageCommand,
+  SQSClient,
+} from "@aws-sdk/client-sqs";
 
-async function enqueue(config, message) {
-  const sqs = new AWS.SQS();
-  const s3 = new AWS.S3();
+/**
+ * Helper function to enqueue a large message to SQS, using S3 to store the message.
+ * @param {*} param0
+ * @param {*} message
+ * @returns
+ */
+export async function enqueue({ sqsQueueName, s3Bucket, s3KeyPrefix }, message) {
+  const sqs = new SQSClient();
+  const s3 = new S3Client();
+  const id = randomBytes(16).toString("hex");
+  const s3Key = `${s3KeyPrefix}${id}.json`;
 
-  const { queueName } = config.sqs;
-  const { bucket, inputKeyPrefix } = config.s3;
+  const { QueueUrl: queueUrl } = await sqs.send(
+    new GetQueueUrlCommand({
+      QueueName: sqsQueueName,
+    }),
+  );
 
-  const id = crypto.randomBytes(16).toString("hex");
-  const s3Key = `${inputKeyPrefix}${id}.json`;
-  const { QueueUrl: queueUrl } = await sqs
-    .getQueueUrl({
-      QueueName: queueName,
-    })
-    .promise();
-
-  await s3
-    .upload({
-      Bucket: bucket,
+  await s3.send(
+    new PutObjectCommand({
+      Bucket: s3Bucket,
       Key: s3Key,
       Body: JSON.stringify(message),
-    })
-    .promise();
+    }),
+  );
 
-  await sqs
-    .sendMessage({
+  await sqs.send(
+    new SendMessageCommand({
       QueueUrl: queueUrl,
       MessageDeduplicationId: id,
       MessageGroupId: id,
       MessageBody: JSON.stringify({
-        bucket,
+        bucket: s3Bucket,
         key: s3Key,
       }),
-    })
-    .promise();
+    }),
+  );
 
   return id;
 }
 
 /**
- * Processes messages from a queue
+ * Processes large messages from a SQS queue which uses S3 as the storage mechanism.
  * @param {*} config
  */
-async function processMessages({
+export async function processMessages({
   queueName,
   visibilityTimeout = 60,
   pollInterval = 60,
@@ -51,56 +62,71 @@ async function processMessages({
   messageHandler,
   errorHandler = console.error,
 }) {
-  const sqs = new AWS.SQS();
+  const s3 = new S3Client();
+  const sqs = new SQSClient();
 
   try {
-    const { QueueUrl: queueUrl } = await sqs
-      .getQueueUrl({
+    const { QueueUrl: queueUrl } = await sqs.send(
+      new GetQueueUrlCommand({
         QueueName: queueName,
-      })
-      .promise();
+      }),
+    );
 
     // to simplify running multiple workers in parallel,
     // fetch one message at a time
-    const data = await sqs
-      .receiveMessage({
+    const data = await sqs.send(
+      new ReceiveMessageCommand({
         QueueUrl: queueUrl,
         MaxNumberOfMessages: 1,
         VisibilityTimeout: visibilityTimeout,
         WaitTimeSeconds: waitTime,
-      })
-      .promise();
+      }),
+    );
 
     if (data.Messages && data.Messages.length > 0) {
       const message = data.Messages[0];
+      const { bucket, key } = JSON.parse(message.Body);
 
       // while processing is not complete, update the message's visibilityTimeout
-      const intervalId = setInterval(
-        () =>
-          sqs
-            .changeMessageVisibility({
-              QueueUrl: queueUrl,
-              ReceiptHandle: message.ReceiptHandle,
-              VisibilityTimeout: visibilityTimeout,
-            })
-            .send(),
-        1000 * (visibilityTimeout * 0.8),
-      );
+      const intervalId = setInterval(async () => {
+        await sqs.send(
+          new ChangeMessageVisibilityCommand({
+            QueueUrl: queueUrl,
+            ReceiptHandle: message.ReceiptHandle,
+            VisibilityTimeout: visibilityTimeout,
+          }),
+        );
+      }, 1000 * (visibilityTimeout / 2));
 
-      // processMessage should return a boolean status indicating success or failure
       try {
-        await messageHandler(message);
+        const s3Response = await s3.send(
+          new GetObjectCommand({
+            Bucket: bucket,
+            Key: key,
+          }),
+        );
+        let messageBody = "";
+        for await (const chunk of s3Response.Body) {
+          messageBody += chunk;
+        }
+        await messageHandler(JSON.parse(messageBody));
       } catch (e) {
         await errorHandler(e, message);
       } finally {
-        // remove original message from queue once processed
+        // remove original message from queue/bucket once processed
         clearInterval(intervalId);
-        await sqs
-          .deleteMessage({
+        await sqs.send(
+          new DeleteMessageCommand({
             QueueUrl: queueUrl,
             ReceiptHandle: message.ReceiptHandle,
-          })
-          .promise();
+          }),
+        );
+        await s3.send(
+          new DeleteObjectCommand({
+            Bucket: bucket,
+            Key: key,
+          }),
+        );
       }
     }
   } catch (e) {
@@ -121,8 +147,3 @@ async function processMessages({
     );
   }
 }
-
-module.exports = {
-  enqueue,
-  processMessages,
-};
