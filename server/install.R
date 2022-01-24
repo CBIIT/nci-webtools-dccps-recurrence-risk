84b9b64996ca825f865cc3b4de642cfe2b876b13
Brian Park <park-brian@users.noreply.github.com>
2022-01-23 20:30:30 -0500

    use renv to manage local R dependencies

--- server/install.R
+++ server/install.R
@@ -1,10 +1,4 @@
-install.packages(
-  c(
-    "jsonlite",
-    "remotes"
-  ), 
-  repos = "https://cloud.r-project.org/"
-)
-
+install.packages("jsonlite", repos = "https://cloud.r-project.org/")
+install.packages("remotes", repos = "https://cloud.r-project.org/")
 remotes::install_github("cran/SEER2R", ref="1.0")
 remotes::install_github("cran/RecurRisk", ref="1.0.2")
