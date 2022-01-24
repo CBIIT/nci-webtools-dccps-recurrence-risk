dc1e1d5ecd0479b72b228258de954dd0054e39e9
Brian Park <park-brian@users.noreply.github.com>
2022-01-23 21:21:03 -0500

    send email with results url

--- server/queue-worker.js
+++ server/queue-worker.js
@@ -1,3 +1,5 @@
+import { randomBytes } from "crypto";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getLogger } from "./services/logger.js";
 import { getSmtpTransport } from "./services/email.js";
 import { processMessages } from "./services/queue.js";
@@ -5,34 +7,54 @@ import { renderTemplate, stringifyCsv } from "./services/utils.js";
 import { recurrence } from "./services/recurrence.js";
 
 const logger = getLogger("recurrence-risk-queue");
-const { SQS_QUEUE_NAME, SQS_VISIBILITY_TIMEOUT, SQS_POLL_INTERVAL, EMAIL_ADMINS, EMAIL_SENDER } = process.env;
+const {
+  APP_BASE_URL,
+  S3_BUCKET,
+  S3_OUTPUT_KEY_PREFIX,
+  SQS_QUEUE_NAME,
+  SQS_VISIBILITY_TIMEOUT,
+  SQS_POLL_INTERVAL,
+  EMAIL_ADMINS,
+  EMAIL_SENDER,
+} = process.env;
 
 processMessages({
   queueName: SQS_QUEUE_NAME,
   visibilityTimeout: SQS_VISIBILITY_TIMEOUT,
   pollInterval: SQS_POLL_INTERVAL,
   messageHandler: async (message) => {
+    const s3 = new S3Client();
     const mailer = getSmtpTransport(process.env);
 
     try {
       logger.info("Retrieved message from SQS queue");
+      const id = randomBytes(16).toString("hex");
       const { version, functionName, params } = message;
       const start = new Date().getTime();
       const results = await recurrence[version][functionName](params);
+      const s3Results = JSON.stringify({ params, results });
+      const s3ResultsKey = `${S3_OUTPUT_KEY_PREFIX}${id}.json`;
       const duration = new Date().getTime() - start;
       logger.info(`Finished calculation in ${duration / 1000}s, sending results`);
+
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: S3_BUCKET,
+          Key: s3ResultsKey,
+          Body: s3Results,
+        }),
+      );
+
+      const emailParams = {
+        ...params,
+        resultsUrl: `${APP_BASE_URL}/#/individual-data/${id}`,
+      };
 
       await mailer.sendMail({
         from: EMAIL_SENDER,
         to: params.email,
         subject: "Recurrence Risk Tool Results",
-        html: await renderTemplate("templates/user-success-email.html", params),
-        attachments: [
-          {
-            filename: "results.csv",
-            content: stringifyCsv(results),
-          },
-        ],
+        html: await renderTemplate("templates/user-success-email.html", emailParams),
       });
 
       logger.info(`Sent results email to: ${params.email}`);
